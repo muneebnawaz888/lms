3468703200457f24cda9648cfa265999fae69e51
Muneeb Nawaz <muneebnawaz888@gmail.com>
2021-03-15 05:45:08 -0700

    update12

--- application/views/login.php
+++ application/views/login.php
@@ -39,7 +39,7 @@
   </div>
   <!-- /.login-logo -->
   <div class="login-box-body">
-    <p class="login-box-msg">Sign in to start your session</p>
+    <p class="login-box-msg" style="color: white;">Sign in to start your session</p>
 
     <?php echo validation_errors(); ?>  
 
