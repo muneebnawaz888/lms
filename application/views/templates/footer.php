5d8507e488b6993cf80205f201ed939dad261cd3
Muneeb Nawaz <muneebnawaz888@gmail.com>
2021-03-15 06:41:10 -0700

    updated

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -3,7 +3,7 @@
     <div class="pull-right hidden-xs">
       <b>Version</b> 0.1.0
     </div>
-    <strong>Copyright &copy; 2018-<?php echo date('Y') ?>.</strong> All rights reserved.
+    <strong></strong> All rights reserved.
   </footer>
 
   <!-- Add the sidebar's background. This div must be placed
