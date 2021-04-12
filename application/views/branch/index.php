94ece39eefb926517677296019712cb305004f2a
Haseeb Nawaz <haseebnawaz298@gmail.com>
2021-04-12 16:37:55 +0500

    add

--- application/views/branch/index.php
+++ application/views/branch/index.php
@@ -1,14 +1,50 @@
- <!-- Data Table area Start-->
-    <div class="data-table-area">
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12" >
-                    <div class="data-table-list  shadow">
-                        <div class="basic-tb-hd">
-                            <h2 class="border_b">Pumps</h2>
-                                    
-                          
-                          <?php if(in_array('createPump', $user_permission)): ?>
+
+
+  <!-- Content Wrapper. Contains page content -->
+  <div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+      <h1>
+        Manage
+        <small>Users</small>
+      </h1>
+      <ol class="breadcrumb">
+        <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
+        <li class="active">Users</li>
+      </ol>
+    </section>
+
+    <!-- Main content -->
+    <section class="content">
+      <!-- Small boxes (Stat box) -->
+      <div class="row">
+        <div class="col-md-12 col-xs-12">
+
+          <?php if($this->session->flashdata('success')): ?>
+            <div class="alert alert-success alert-dismissible" role="alert">
+              <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+              <?php echo $this->session->flashdata('success'); ?>
+            </div>
+          <?php elseif($this->session->flashdata('error')): ?>
+            <div class="alert alert-error alert-dismissible" role="alert">
+              <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+              <?php echo $this->session->flashdata('error'); ?>
+            </div>
+          <?php endif; ?>
+          
+          <?php if(in_array('createUser', $user_permission)): ?>
+            <a href="<?php echo base_url('users/create') ?>" class="btn btn-primary">Add User</a>
+            <br /> <br />
+          <?php endif; ?>
+
+
+          <div class="box">
+            <div class="box-header">
+              <h3 class="box-title">Manage Users</h3>
+            </div>
+            <!-- /.box-header -->
+            <div class="box-body">
+              <?php if(in_array('createBranch', $user_permission)): ?>
                            
                           <button class="btn btn-shadow add-new" data-toggle="modal" data-target="#addpump"><i class="fa fa-plus"></i></button>
                             
@@ -19,12 +55,30 @@
                         <div class="table-responsive" id="show">
                         
                         </div>
-                    </div>
-                </div>
             </div>
+            <!-- /.box-body -->
+          </div>
+          <!-- /.box -->
         </div>
-    </div>
-  <!-- Modal -->
+        <!-- col-md-12 -->
+      </div>
+      <!-- /.row -->
+      
+
+    </section>
+    <!-- /.content -->
+  </div>
+  <!-- /.content-wrapper -->
+
+  <script type="text/javascript">
+    $(document).ready(function() {
+      $('#userTable').DataTable();
+
+      $("#mainUserNav").addClass('active');
+      $("#manageUserNav").addClass('active');
+    });
+  </script>
+ <!-- Modal -->
 <div class="modal fade"  id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
   <div class="modal-dialog" role="document">
     <div class="modal-content">
