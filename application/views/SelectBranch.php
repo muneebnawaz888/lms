ab5629818913acca4e7322aa6b8fcfd2d500df31
Haseeb Nawaz <haseebnawaz298@gmail.com>
2021-04-12 22:56:37 +0500

    sdsds

--- application/views/SelectBranch.php
+++ application/views/SelectBranch.php
@@ -1,75 +1,41 @@
-<!doctype html>
-<html class="no-js" lang="">
-
+<!DOCTYPE html>
+<html>
 <head>
-    <meta charset="utf-8">
-    <meta http-equiv="x-ua-compatible" content="ie=edge">
-    <meta name="viewport" value="width=device-width, initial-scale=1.0">
-    <title>Select Pump</title>
-    <meta name="description" content="">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- favicon
-    ============================================ -->
-    <link rel="shortcut icon" type="image/x-icon" href="img/favicon.ico">
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title>Log in</title>
+  <!-- Tell the browser to be responsive to screen width -->
+  <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+  <!-- Bootstrap 3.3.7 -->
+  
+  
+  <link rel="stylesheet" href="<?php echo base_url('assets/bower_components/bootstrap/dist/css/bootstrap.min.css') ?>">
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="<?php echo base_url('assets/bower_components/font-awesome/css/font-awesome.min.css') ?>">
+  <!-- Ionicons -->
+  <link rel="stylesheet" href="<?php echo base_url('assets/bower_components/Ionicons/css/ionicons.min.css') ?>">
+  <!-- Theme style -->
+  <link rel="stylesheet" href="<?php echo base_url('assets/dist/css/AdminLTE.min.css') ?>">
+  <!-- iCheck -->
+  <link rel="stylesheet" href="<?php echo base_url('assets/plugins/iCheck/square/blue.css') ?>">
+  <link rel="stylesheet" href="<?php echo base_url('assets/style.css') ?>">
 
-      <link rel="stylesheet" href="<?php echo base_url('assets/css/custom.css'); ?>">
+ 
 
-    <!-- Google Fonts
-    ============================================ -->
-    <link href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,700,900" rel="stylesheet">
-    <!-- Bootstrap CSS
-    ============================================ -->
-    <link rel="stylesheet" href="<?php echo base_url('assets/css/bootstrap.min.css'); ?>">
-    <!-- Bootstrap CSS
-    ============================================ -->
-    <link rel="stylesheet" href="<?php echo base_url('assets/css/font-awesome.min.css'); ?>">
-    <!-- owl.carousel CSS
-    ============================================ -->
-    <link rel="stylesheet" href="<?php echo base_url('assets/css/owl.carousel.css'); ?>">
-    <link rel="stylesheet" href="<?php echo base_url('assets/css/owl.theme.css'); ?>">
-    <link rel="stylesheet" href="<?php echo base_url('assets/css/owl.transitions.css'); ?>">
-    <!-- meanmenu CSS
-    ============================================ -->
-    <link rel="stylesheet" href="<?php echo base_url('assets/css/meanmenu/meanmenu.min.css'); ?>">
-    <!-- animate CSS
-    ============================================ -->
-    <link rel="stylesheet" href="<?php echo base_url('assets/css/animate.css'); ?>">
-    <!-- normalize CSS
-    ============================================ -->
-    <link rel="stylesheet" href="<?php echo base_url('assets/css/normalize.css'); ?>">
-    <!-- mCustomScrollbar CSS
-    ============================================ -->
-    <link rel="stylesheet" href="<?php echo base_url('assets/css/scrollbar/jquery.mCustomScrollbar.min.css'); ?>">
-    <!-- jvectormap CSS
-    ============================================ -->
-    <link rel="stylesheet" href="<?php echo base_url('assets/css/jvectormap/jquery-jvectormap-2.0.3.css'); ?>">
-    <!-- notika icon CSS
-    ============================================ -->
-    <link rel="stylesheet" href="<?php echo base_url('assets/css/notika-custom-icon.css'); ?>">
-    <!-- wave CSS
-    ============================================ -->
-    <link rel="stylesheet" href="<?php echo base_url('assets/css/wave/waves.min.css'); ?>">
-    <!-- main CSS
-    ============================================ -->
-    <link rel="stylesheet" href="<?php echo base_url('assets/css/main.css'); ?>">
-    <!-- style CSS
-    ============================================ -->
-    <link rel="stylesheet" href="<?php echo base_url('assets/style.css'); ?>">
-    <!-- responsive CSS
-    ============================================ -->
-    <link rel="stylesheet" href="<?php echo base_url('assets/css/responsive.css'); ?>">
+  <!-- Google Font -->
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
 
-    <!-- modernizr JS
-    ============================================ -->
-    <script src="<?php echo base_url('assets/js/vendor/modernizr-2.8.3.min.js'); ?>"></script>
 </head>
-
-<body>
-   
-    
-    
-    <br><br><br><br><br>
-<div class="notika-status-area">
+<body class="hold-transition login-page">
+<div class="login-box">
+  <div class="login-logo">
+    <img class="logo" src="<?php echo base_url('assets/images/lms_image/logo.png') ?>"><br>
+    <a href="<?php echo base_url('auth'); ?>" style="color: white; "><b>LMS System</b><br><b style="font-size: 20px;">with Automated TimeTable</b></a>
+  </div>
+  <!-- /.login-logo -->
+  <div class="login-box-body">
+    <p class="login-box-msg" style="color: white;">login in to start your session</p>
+    <div class="notika-status-area">
         <div class="container">
             <div class="row">
 
@@ -138,6 +104,27 @@ $user_id = $this->session->userdata('id');
         </div>
     </div>
 
+  </div>
+  <!-- /.login-box-body -->
+</div>
+<!-- /.login-box -->
+
+<!-- jQuery 3 -->
+
+<script src="<?php echo base_url('assets/bower_components/jquery/dist/jquery.min.js') ?>"></script>
+<!-- Bootstrap 3.3.7 -->
+<script src="<?php echo base_url('assets/bower_components/bootstrap/dist/js/bootstrap.min.js') ?>"></script>
+<!-- iCheck -->
+<script src="<?php echo base_url('assets/plugins/iCheck/icheck.min.js') ?>"></script>
+<script>
+  $(function () {
+    $('input').iCheck({
+      checkboxClass: 'icheckbox_square-blue',
+      radioClass: 'iradio_square-blue',
+      increaseArea: '20%' // optional
+    });
+  });
+</script>
 </body>
 </html>
  <!-- Modal -->
@@ -199,74 +186,3 @@ $user_id = $this->session->userdata('id');
     </div>
   </div>
 </div>
-
-    <!-- jquery
-    ============================================ -->
-    <script src="<?php echo base_url('assets/js/vendor/jquery-1.12.4.min.js'); ?>"></script>
-    <!-- bootstrap JS
-    ============================================ -->
-    <script src="<?php echo base_url('assets/js/bootstrap.min.js'); ?>"></script>
-    <!-- wow JS
-    ============================================ -->
-    <script src="<?php echo base_url('assets/js/wow.min.js'); ?>"></script>
-    <!-- price-slider JS
-    ============================================ -->
-    <script src="<?php echo base_url('assets/js/jquery-price-slider.js'); ?>"></script>
-    <!-- owl.carousel JS
-    ============================================ -->
-    <script src="<?php echo base_url('assets/js/owl.carousel.min.js'); ?>"></script>
-    <!-- scrollUp JS
-    ============================================ -->
-    <script src="<?php echo base_url('assets/js/jquery.scrollUp.min.js'); ?>"></script>
-    <!-- meanmenu JS
-    ============================================ -->
-    <script src="<?php echo base_url('assets/js/meanmenu/jquery.meanmenu.js'); ?>"></script>
-    <!-- counterup JS
-    ============================================ -->
-    <script src="<?php echo base_url('assets/js/counterup/jquery.counterup.min.js'); ?>"></script>
-    <script src="<?php echo base_url('assets/js/counterup/waypoints.min.js'); ?>"></script>
-    <script src="<?php echo base_url('assets/js/counterup/counterup-active.js'); ?>"></script>
-    <!-- mCustomScrollbar JS
-    ============================================ -->
-    <script src="<?php echo base_url('assets/js/scrollbar/jquery.mCustomScrollbar.concat.min.js'); ?>"></script>
-    <!-- jvectormap JS
-    ============================================ -->
-    <script src="<?php echo base_url('assets/js/jvectormap/jquery-jvectormap-2.0.2.min.js'); ?>"></script>
-    <script src="<?php echo base_url('assets/js/jvectormap/jquery-jvectormap-world-mill-en.js'); ?>"></script>
-    <script src="<?php echo base_url('assets/js/jvectormap/jvectormap-active.js'); ?>"></script>
-    <!-- sparkline JS
-    ============================================ -->
-    <script src="<?php echo base_url('assets/js/sparkline/jquery.sparkline.min.js'); ?>"></script>
-    <script src="<?php echo base_url('assets/js/sparkline/sparkline-active.js'); ?>"></script>
-    <!-- sparkline JS
-    ============================================ -->
-    <script src="<?php echo base_url('assets/js/flot/jquery.flot.js'); ?>"></script>
-    <script src="<?php echo base_url('assets/js/flot/jquery.flot.resize.js'); ?>"></script>
-    <script src="<?php echo base_url('assets/js/flot/curvedLines.js'); ?>"></script>
-    <script src="<?php echo base_url('assets/js/flot/flot-active.js'); ?>"></script>
-    <!-- knob JS
-    ============================================ -->
-    <script src="<?php echo base_url('assets/js/knob/jquery.knob.js'); ?>"></script>
-    <script src="<?php echo base_url('assets/js/knob/jquery.appear.js'); ?>"></script>
-    <script src="<?php echo base_url('assets/js/knob/knob-active.js'); ?>"></script>
-    <!--  wave JS
-    ============================================ -->
-    <script src="<?php echo base_url('assets/js/wave/waves.min.js'); ?>"></script>
-    <script src="<?php echo base_url('assets/js/wave/wave-active.js'); ?>"></script>
-    <!--  todo JS
-    ============================================ -->
-    <script src="<?php echo base_url('assets/js/todo/jquery.todo.js'); ?>"></script>
-    <!-- plugins JS
-    ============================================ -->
-    <script src="<?php echo base_url('assets/js/plugins.js'); ?>"></script>
-  <!--  Chat JS
-    ============================================ -->
-    <script src="<?php echo base_url('assets/js/chat/moment.min.js'); ?>"></script>
-    <script src="<?php echo base_url('assets/js/chat/jquery.chat.js'); ?>"></script>
-    <!-- main JS
-    ============================================ -->
-    <script src="<?php echo base_url('assets/js/main.js'); ?>"></script>
-  <!-- tawk chat JS
-    ============================================ -->
-    
-
