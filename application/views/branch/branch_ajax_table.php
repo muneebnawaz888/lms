25a824fd337b8d16f723b7e01cd5a280eeb13317
Haseeb Nawaz <haseebnawaz298@gmail.com>
2021-04-12 17:46:40 +0500

    add

--- application/views/branch/branch_ajax_table.php
+++ application/views/branch/branch_ajax_table.php
@@ -1,27 +1,25 @@
     <table id="mytable" class="table table-striped">
                            <thead>
                 <tr>
-                  <th>Pump Name</th>
-                  <th>Pump Address</th>
-                  <?php if(in_array('updatePump', $user_permission) || in_array('deletePump', $user_permission)): ?>
+                  <th>Branch Name</th>
+                  <?php if(in_array('updateBranch', $user_permission) || in_array('deleteBranch', $user_permission)): ?>
                     <th>Action</th>
                   <?php endif; ?>
                 </tr>
                 </thead>
                           <tbody  >
            <?php
-            foreach ($pump_data as $pump_key => $pump_value): ?>
+            foreach ($branch_data as $branch_key => $branch_value): ?>
             <tr>
                
-                <td><?php echo $pump_value['pump_name'] ?></td>
-                <td><?php echo $pump_value['pump_address'] ?></td>
-                <?php if(in_array('updatePump', $user_permission) || in_array('deletePump', $user_permission)): ?>
+                <td><?php echo $branch_value['branch_name'] ?></td>
+                <?php if(in_array('updateBranch', $user_permission) || in_array('deleteBranch', $user_permission)): ?>
                         <td>
-                          <?php if(in_array('updatePump', $user_permission)): ?>
+                          <?php if(in_array('updateBranch', $user_permission)): ?>
                          <button data-id="<?php echo $pump_value['pump_id'] ?>" data-name="<?php echo $pump_value['pump_name'] ?>" data-address="<?php echo $pump_value['pump_address'] ?>" class="btn btn-default Edit" data-toggle="modal" data-target="#editpump"><i class="fa fa-edit"></i></button>
         
                           <?php endif; ?>
-                         <!-- <?php if(in_array('deletePump', $user_permission)): ?>-->
+                         <!-- <?php if(in_array('deleteBranch', $user_permission)): ?>-->
                           
                          <!--<button data-id="<?php echo $pump_value['pump_id'] ?>" class="btn btn-default delete" data-toggle="modal" data-target="#exampleModal"><i class="fa fa-trash"></i></button>-->
                 
