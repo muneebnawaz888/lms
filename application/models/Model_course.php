555f2d52d2259546582c0c0a715b53750b83d7a0
Haseeb Nawaz <haseebnawaz298@gmail.com>
2021-05-09 23:10:02 +0500

    Model_course

--- application/models/Model_course.php
+++ application/models/Model_course.php
@@ -6,4 +6,46 @@ class Model_course extends CI_Model
 	{
 		parent::__construct();
 	}
+		public function getCourseData($id = null) 
+	{
+		if($id) {
+			$sql = "SELECT * FROM course WHERE course_id = '$id'";
+			$query = $this->db->query($sql);
+			return $query->result_array();
+		}else{
+			$sql = "SELECT * FROM course";
+			$query = $this->db->query($sql);
+			return $query->result_array();
+
+		}
+
+		
+	}
+
+	public function create($data,$acc_array=null)
+	{
+		if($data) {
+			
+			$create = $this->db->insert('course', $data);
+			$course_id=$this->db->insert_id();
+			// foreach ($acc_array as $key => $value) {
+
+			// 	$value['course_id']=$course_id;
+			// 	$this->db->insert('account_type', $value);
+			// }
+			return ($create == true) ? true : false;
+		}
+	}
+	public function delete($id)
+	{
+		$query="DELETE FROM `course` WHERE course_id='$id'";
+		$delete=$this->db->query($query);
+		return ($delete == true) ? true : false;
+	}
+	public function edit($data, $id)
+	{
+		$this->db->where('course_id', $id);
+		$update = $this->db->update('course', $data);
+		return ($update == true) ? true : false;	
+	}
 }
